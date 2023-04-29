1563eba2b2e6a71eda4c5bf1737bad57921629fa
danlhennessy <danh118@hotmail.co.uk>
2023-04-29 20:51:29 +0100

    CDK base

---
+++ ToDo.md
@@ -0,0 +1,8 @@
+New project to add to portfolio:
+
+
+Plan outline
+- AWS CDK
+- Debugging tests while building
+- Include unit / function / integration tests
+- Secrets Manager for secrets
--- aws_cdk_deploy/aws_cdk_deploy_stack.py
+++ aws_cdk_deploy/aws_cdk_deploy_stack.py
@@ -1,19 +1,15 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
 
-class AwsCdkDeployStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+class AwsCdkDeployStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkDeployQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(
+            self, 
+            "MyFirstBucket",
+            versioned=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True)
